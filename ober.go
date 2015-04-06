b1d49fde5a3eed455591218ffa50eb5516ca9b2e
Vincent Swarte <vincent@vinsert.nl>
2015-04-06 13:38:57 +0200

    Fixed tabbing

--- ober.go
+++ ober.go
@@ -1,9 +1,9 @@
 package main
 
 import(
-	"flag"
-	"net/http"
-	"fmt"
+  "flag"
+  "net/http"
+  "fmt"
   "strconv"
 )
 
@@ -11,22 +11,22 @@ func main(){
 
   //Flags, invoked with $ ober --help
   verbose := flag.Bool( "verbose", false, "turns on verbose mode" )
-	fileName := flag.String( "file", "", "used to specify the file you want to serve (required)" )
+  fileName := flag.String( "file", "", "used to specify the file you want to serve (required)" )
   port := flag.Int( "port", 3232, "used to specify the port you want to serve on (defaults to 3232)" )
 
-	flag.Parse()
+  flag.Parse()
 
   if *fileName == ""{
     panic( "You should atleast specify a file" )
   }
 
-	if *verbose{
-		fmt.Printf( "Serving file: %s", *fileName )
-	}
+  if *verbose{
+    fmt.Printf( "Serving file: %s", *fileName )
+  }
 
-	http.HandleFunc( "/", func( res http.ResponseWriter, req *http.Request ){
-		http.ServeFile( res, req, *fileName )
-	})
+  http.HandleFunc( "/", func( res http.ResponseWriter, req *http.Request ){
+    http.ServeFile( res, req, *fileName )
+  })
 
-	panic(http.ListenAndServe(":" + strconv.Itoa( *port ), nil))
+  panic(http.ListenAndServe(":" + strconv.Itoa( *port ), nil))
 }
